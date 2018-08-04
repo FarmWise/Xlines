a05d4a98cb953fb396bd91c56e445c7ce81afe35
Sebastien <sdubois.sebastien@gmail.com>
2018-08-03 21:07:48 -0700

    clean API to match sklearn standards

--- xlines/klines.py
+++ xlines/klines.py
@@ -6,31 +6,59 @@ from sklearn.metrics import silhouette_score, calinski_harabaz_score
 
 class KLines(object):
     
-    def __init__(self, n_components, metric="silhouette", verbose=2):
+    def __init__(self, n_components, alpha0=0., init_alpha=True, max_iter=10, 
+                 tol=0.001, clustering_n_init=3, clustering_init="estimate", 
+                 metric="silhouette", verbose=0):
         """
         Initialize the KLines object
 
         Parameters
         ---
         n_components : int, number of lines to fit
+        alpha0 : float, initial value for alpha if init_alpha is False
+        init_alpha : boolean, if True test a few alpha0  and initialize 
+            the iterations with the best one
+        max_iter : int, maximum number of iteration
+        tol : float, tolerance to stop convergence
+        clustering_n_init : int, number of times the KMeans algorithms will
+            be run with different centroid seeds (n_init for KMeans)
+        clustering_init : {'estimate' or 'k-means++'}, initialization method
+            for KMeans. 'k-means++' is the standard for KMeans while 'estimate'
+            will use the centroids from the previous step
         metric : ther metric to use for scoring, either 'silhouette' or 'CH'
+        verbose : {0, 1, 2}, the level of verbosity
+
+        Attributes
+        ---
+        alpha_ : line orientation in radians
+        labels_ : labels (cluster index) of each training points
+        centroids_ : coordinates of the cluster centers
+        n_iter_ : number of iterations needed to fit the algorithm
+        score_ : a score to evaluate the training data clustering
         """
-        self.alpha = None
         self.n_components = n_components
+        self.alpha0 = alpha0
+        self.init_alpha = init_alpha
+        self.max_iter = max_iter
+        self.tol = tol
+        self.clustering_n_init = clustering_n_init
+        self.clustering_init = clustering_init
         self.metric = metric
         self.verbose = verbose
         
-        self._labels = None
-        self._centers = None
-        self._Xprojected = None
-
-        self.clustering_n_init_ = None
-        self.clustering_init_ = None
+        self.alpha__ = None
+        self.labels_ = None
+        self.centroids_ = None
+        self.n_iter_ = None
         self.score_ = None
 
+        self._Xprojected = None
+
         # check inputs
-        if not(metric in ["silhouette", "CH"]):
+        if not(self.metric in ["silhouette", "CH"]):
             raise ValueError("metric should be 'silhouette' or 'CH'")
+        if not(self.clustering_init in ["k-means++", "estimate"]):
+            raise ValueError("clustering_init should be either 'k-means++' or 'estimate' - got {} instead".format(clustering_init))
 
 
     def _cluster(self, X, alpha, store, verbose):
@@ -57,13 +85,13 @@ class KLines(object):
             plt.show()
 
 
-        if self.clustering_init_ == "estimate" and self._centers is not None:
-            init_centers = np.dot(R, self._centers.T).T
+        if self.clustering_init == "estimate" and self.centroids_ is not None:
+            init_centers = np.dot(R, self.centroids_.T).T
             init_centers = init_centers[:,1].reshape(-1, 1)
             n_init = 1
         else:
             init_centers = "k-means++"
-            n_init = self.clustering_n_init_
+            n_init = self.clustering_n_init
 
         alg = KMeans(n_clusters=self.n_components, init=init_centers, n_init=n_init)
         alg.fit(projX)
@@ -72,13 +100,13 @@ class KLines(object):
 
 
         if store:
-            self._labels = labels
+            self.labels_ = labels
             self._Xprojected = projX
         
-        if self.clustering_init_ == "estimate" or store:
+        if self.clustering_init == "estimate" or store:
             projCenters = np.zeros((self.n_components, 2))
             projCenters[:,1] = alg.cluster_centers_[:,0]
-            self._centers = np.dot(R.T, projCenters.T).T
+            self.centroids_ = np.dot(R.T, projCenters.T).T
 
         if verbose:
             print("Kmeans w/ proj {}: {}".format(utils.rad2deg(alpha), score))
@@ -121,7 +149,7 @@ class KLines(object):
         alpha_diff : orientation update
         """
         
-        labels, score = self._cluster(X, self.alpha, store=True, verbose=self.verbose)
+        labels, score = self._cluster(X, self.alpha_, store=True, verbose=self.verbose)
 
         orientations = []
         for c in xrange(self.n_components):
@@ -130,28 +158,18 @@ class KLines(object):
                 orientations.append(a)
 
         alpha = np.mean(orientations)
-        alpha_diff = abs(alpha-self.alpha)
-        self.alpha = alpha
+        alpha_diff = abs(alpha-self.alpha_)
+        self.alpha_ = alpha
         return alpha_diff
 
 
-    def fit(self, X, alpha0=0., init_alpha=True, max_iter=10, tol=0.001, clustering_n_init=3, clustering_init="estimate"):
+    def fit(self, X):
         """
         Fitting algorithm: cluster data X in K lines
 
         Parameters
         ---
         X : array-like, shape (n_samples, n_features)
-        alpha0 : float, initial value for alpha if init_alpha is False
-        init_alpha : boolean, if True test a few alpha0  and initialize 
-            the iterations with the best one
-        max_iter : int, maximum number of iteration
-        tol : float, tolerance to stop convergence
-        clustering_n_init : int, number of times the KMeans algorithms will
-            be run with different centroid seeds (n_init for KMeans)
-        clustering_init : {'estimate' or 'k-means++'}, initialization method
-            for KMeans. 'k-means++' is the standard for KMeans while 'estimate'
-            will use the centroids from the previous step
 
         Returns
         ---
@@ -161,40 +179,35 @@ class KLines(object):
         # init
         X = np.asarray(X)
         self.score_ = None
-        self.clustering_n_init_ = clustering_n_init
-        self.clustering_init_ = clustering_init
-        if not(self.clustering_init_ in ["k-means++", "estimate"]):
-            raise ValueError("clustering_init should be either 'k-means++' or 'estimate' - got {} instead".format(clustering_init))
-
 
         # initialize alpha
-        if init_alpha:
-            self.alpha = self._init_alpha(X)
+        if self.init_alpha:
+            self.alpha_ = self._init_alpha(X)
         else:
-            self.alpha = alpha0
+            self.alpha_ = self.alpha0
 
 
-        alpha_diff = 100. + tol # something larger than tol
-        n_iter = 0
-        while(alpha_diff > tol and n_iter < max_iter):
+        alpha_diff = 100. + self.tol # something larger than tol
+        self.n_iter_ = 0
+        while(alpha_diff > self.tol and self.n_iter_ < self.max_iter):
             alpha_diff = self._fit_step(X)
-            n_iter += 1
+            self.n_iter_ += 1
 
-        if alpha_diff > tol and self.verbose:
+        if alpha_diff > self.tol and self.verbose:
             print("[Warning KLines {}] Fit did not converge but maximum number of iteration reached".format(self.n_components))
 
-        return self.alpha
+        return self
 
 
     def score(self):
         """
         Score of the KMeans clustering on the projected axis
         """
-        if self.score_ is None and self._labels is not None:
+        if self.score_ is None and self.labels_ is not None:
             if self.metric == "CH":
-                self.score_ = calinski_harabaz_score(self._Xprojected, self._labels)
+                self.score_ = calinski_harabaz_score(self._Xprojected, self.labels_)
             elif self.metric == "silhouette":
-                self.score_ = silhouette_score(self._Xprojected, self._labels, metric="euclidean")
+                self.score_ = silhouette_score(self._Xprojected, self.labels_, metric="euclidean")
             else:
                 self.score_ = None
         return self.score_
@@ -213,12 +226,12 @@ class KLines(object):
         labels : array, index of the cluster each sample belongs to
         """
 
-        if self._centers is None:
+        if self.centroids_ is None:
             raise RuntimeError("Model needs to be fitted first")
 
-        R = utils.rotation_matrix(-self.alpha)
+        R = utils.rotation_matrix(-self.alpha_)
         projX = (np.dot(R, np.asarray(X).T).T)[:,1]
-        projCentroids = (np.dot(R, self._centers.T).T)[:,1]
+        projCentroids = (np.dot(R, self.centroids_.T).T)[:,1]
 
         labels = [np.argmin([abs(x - c) for c in projCentroids]) for x in projX]
 
--- xlines/xlines.py
+++ xlines/xlines.py
@@ -6,38 +6,15 @@ import numpy as np
 
 class XLines(object):
     
-    def __init__(self, candidates, metric="silhouette", verbose=2):
+    def __init__(self, candidates, alpha0=0., init_alpha="one", max_iter=10, tol=0.001, 
+                 clustering_n_init=3, clustering_init="estimate", metric="silhouette", 
+                 verbose=0):
         """
         Initialize a XLines object
 
         Parameters
         ---
         candidates : array-like, list of candidate k for which we will try to fit a KLines
-        metric : ther metric to use for scoring, either 'silhouette' or 'CH'
-        """
-        self.candidates = candidates
-        self.n_candidates = len(candidates)
-        self.metric = metric
-        self.models = None
-        self.verbose = verbose
-
-        self._scores = None
-
-        self.best_k_ = None
-        self.best_model_ = None
-
-        # check inputs
-        if not(metric in ["silhouette", "CH"]):
-            raise ValueError("metric should be 'silhouette' or 'CH'")
-
-
-    def fit(self, X, alpha0=0., init_alpha="one", max_iter=10, tol=0.001, clustering_n_init=3, clustering_init="estimate"):
-        """
-        Fitting algorithm: fit a KLines on X for each candidate k
-
-        Parameters
-        ---
-        X : array-like, shape (n_samples, n_features)
         alpha0 : float, initial value for alpha if init_alpha is None or "update"
         init_alpha : {None, "one", "all", "update"}, if not None test a few alpha0 and 
             initialize the iterations with the best one.
@@ -52,71 +29,109 @@ class XLines(object):
         clustering_init : {'estimate' or 'k-means++'}, initialization method
             for KMeans. 'k-means++' is the standard for KMeans while 'estimate'
             will use the centroids from the previous step
+        metric : ther metric to use for scoring, either 'silhouette' or 'CH'
+
+        Attributes
+        ---
+        best_k_ : best candidate for the number of lines
+        best_model_ : the best KLines model
+        scores_ : a list of each candidate model scores
+        """
+        self.candidates = candidates
+        self.n_candidates = len(candidates)
+        self.alpha0 = alpha0
+        self.init_alpha = init_alpha
+        self.max_iter = max_iter
+        self.tol = tol
+        self.clustering_n_init = clustering_n_init
+        self.clustering_init = clustering_init
+        self.metric = metric
+        self.verbose = verbose
+
+        self.best_k_ = None
+        self.best_model_ = None
+        self.scores_ = None
+
+        self._alpha0 = self.alpha0
+
+        # check inputs
+        if not(metric in ["silhouette", "CH"]):
+            raise ValueError("metric should be 'silhouette' or 'CH'")
+        if not(self.clustering_init in ["k-means++", "estimate"]):
+            raise ValueError("clustering_init should be either 'k-means++' or 'estimate' - got {} instead".format(clustering_init))
+
+
+    def fit(self, X):
+        """
+        Fitting algorithm: fit a KLines on X for each candidate k
+
+        Parameters
+        ---
+        X : array-like, shape (n_samples, n_features)
 
         Returns
         ---
         best_model : a fitted KLines model that corresponds to the highest score
         best_k : the candidate k yielding the best model
         """
-
-        # reset scores
-        self._scores = []
-
-        # initialize models
+        self.scores_ = []
         sub_verbose = max(0, self.verbose-1)
-        self.models = dict((k, KLines(k, metric=self.metric, verbose=sub_verbose)) for k in self.candidates)
+        models = {} 
 
         # alpha_initializations
-        if init_alpha is None or init_alpha == "update":
+        if self.init_alpha is None or self.init_alpha == "update":
             alpha_initializations = [False] * self.n_candidates
-        elif init_alpha == "one":
+        elif self.init_alpha == "one":
             alpha_initializations = [True] + [False] * (self.n_candidates - 1)
-        elif init_alpha == "all":
+        elif self.init_alpha == "all":
             alpha_initializations = [True] * self.n_candidates
         else:
-            raise ValueError("init_alpha must be None, 'one', 'all' or 'update' - got {} instead".format(init_alpha))
+            raise ValueError("init_alpha must be None, 'one', 'all' or 'update' - got {} instead".format(self.init_alpha))
 
         # update_alpha0 after the first candidate if necessary
-        if init_alpha == "one" or init_alpha == "update":
+        if self.init_alpha == "one" or self.init_alpha == "update":
             update_alpha0 = True
         else:
             update_alpha0 = False
 
         # maximum number of iterations for each candidate
-        if type(max_iter) is int:
-            max_iterations = [max_iter] * self.n_candidates
-        elif len(max_iter) != self.n_candidates:
+        if type(self.max_iter) is int:
+            max_iterations = [self.max_iter] * self.n_candidates
+        elif len(self.max_iter) != self.n_candidates:
             raise ValueError("max_iter should be an integer or a list of the same size of the number of candidates")
         else:
-            max_iterations = max_iter
+            max_iterations = self.max_iter
+
 
         # fit a KLines model for each candidate k
         for idx, k in enumerate(self.candidates):
-
             if self.verbose > 1:
                 print("-Test {} components".format(k))
 
-            model = self.models[k]
-            model.fit(
-                X, alpha0=alpha0, init_alpha=alpha_initializations[idx], max_iter=max_iterations[idx], 
-                tol=tol, clustering_n_init=clustering_n_init, clustering_init=clustering_init
+            model = KLines(k, 
+                alpha0=self._alpha0, init_alpha=alpha_initializations[idx], 
+                max_iter=max_iterations[idx], tol=self.tol, 
+                clustering_n_init=self.clustering_n_init, clustering_init=self.clustering_init, 
+                metric=self.metric, verbose=sub_verbose
             )
-            self._scores.append(model.score())
+            model.fit(X)
+            models[k] = model
+            self.scores_.append(model.score())
 
              # update_alpha0 after the first candidate if necessary
             if update_alpha0:
-                alpha0 = model.alpha
+                self._alpha0 = model.alpha_
                 update_alpha0 = False
 
         # select best model
-        self.best_k_ = self.candidates[np.argmax(self._scores)]
-        self.best_model_ = self.models[self.best_k_]
+        self.best_k_ = self.candidates[np.argmax(self.scores_)]
+        self.best_model_ = models[self.best_k_]
 
         if self.verbose:
             print("-Results:")
-            print("Candidate scores: {}".format(self._scores))
-            print("Best model with {} components".format(best_k))
-            print("Best model with orientation: {:.2f}".format(utils.rad2deg(best_model.alpha)))
+            print("Candidate scores: {}".format(self.scores_))
+            print("Best model with {} components".format(self.best_k_))
+            print("Best model with orientation: {:.2f}".format(utils.rad2deg(self.best_model_.alpha_)))
             
         return self.best_model_, self.best_k_
 
